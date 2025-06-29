58470dc6da3a8b42a67e47598bfc6d041f41d99b
LE1NART <Linus@von-Au.de>
2025-06-29 17:07:13 +0200

    small changes

--- javascript/headerScroll.js
+++ javascript/headerScroll.js
@@ -10,13 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   const unternehmenLink = document.getElementById("unternehmenLink");
+  const ulink = document.getElementById("ulink");
   const subMenu = unternehmenLink.querySelector(".subMenu");
 
   unternehmenLink.addEventListener("mouseenter", () => {
     subMenu.classList.add("showed");
+    ulink.classList.add("hoverFake");
   });
 
   unternehmenLink.addEventListener("mouseleave", () => {
     subMenu.classList.remove("showed");
+    ulink.classList.remove("hoverFake");
   });
 });
